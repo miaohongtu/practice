0cf85e2c60cdd6afda28f3abfa42e25f03a2c570
miaohongtu <miaohongtu@jd.com>
2017-12-04 09:00:39 +0800

    second

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require("express");
+var bodyParser = require("body-parser");
 let app = new express();
+app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname));
 app.get("/",function(req,res,next){
     res.sendFile('index.html');
@@ -13,6 +15,11 @@ app.get("/submit",function(req,res,next){
 app.get("/toast",function(req,res,next){
     res.sendFile(__dirname + '/toast.html');
 })
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.post("/ha",urlencodedParser,function(req,res,next){
+    console.log(req.body);
+    res.send("henhao");
+});
 app.listen(8000,"192.168.43.115",function(){
     console.log("Server is opening!");
 });
